f3bd0918096a50f7cb8a5aa34df6cc096cec5d22
yagyeshgoyal <yagyeshgoyal123@gmail.com>
2025-02-25 19:38:42 +0530

    Refactor DeliveryDiscount class to implement family interface and remove unused methods

--- Low-Level-Design/1-SolidPrinciples/LiskovSubstitutionPrinciple/DeliveryDiscount.java
+++ Low-Level-Design/1-SolidPrinciples/LiskovSubstitutionPrinciple/DeliveryDiscount.java
@@ -1,66 +1,37 @@
 public class DeliveryDiscount{
 
-    public static void finalTotalAmount(Order o, float dis){
-        ChangeTotalAount c = new ChangeTotalAount();
-        c.getDiscount(o, dis);
-        System.out.println("Total Amount: "+o.getTotalAmount());
-    }
-
     public static void main(String[] args){
 
-        Order o = new Order(1000, 50);
-        System.out.println("Initial Total Amount: "+o.getTotalAmount());
-        FreeDelivery f1 = new FreeDelivery();
-        finalTotalAmount(o, f1.getDiscount(o));
-        Order o1 = new Order(7000, 50);
-        System.out.println("Initial Total Amount: "+o1.getTotalAmount());
-        finalTotalAmount(o1, f1.getDiscount(o1));
+        family f1 = new Family1();
+        f1.familyType();
+        family f2 = new Family2(); 
+        f2.familyType();
         
-
-
     }
 }
 
 
 class Order{
-    float price;
-    int deliverycharge;
-    float totalAmount;
-
-    public Order (int price,int deliverycharge){
-        this.price = price;
-        this.deliverycharge = deliverycharge;
-        this.totalAmount = price+deliverycharge;
-    }
-
-    public float getTotalAmount(){
-        return totalAmount;
-    }
+    String food;
 }
 
-class ChangeTotalAount {
+interface family{
 
-    public void getDiscount(Order o, float dis){
-        o.totalAmount = o.totalAmount - dis;
-    }
+    public void familyType();
     
 }
 
-
-interface Discount{
-
-    public float getDiscount(Order o);
-    
-}
-
-class FreeDelivery implements Discount{
+class Family1 implements family{
     @Override
-    public float getDiscount(Order o){
-        if(o.totalAmount > 5000){
-            return o.deliverycharge;
-        }
+    public void familyType(){
+        System.out.println("Family1 is Non-veg");
+    }
+}
 
-        return 0;
+class Family2 implements family{
+    @Override
+    public void familyType(){
+        System.out.println("Family2 is veg");
     }
 }
 
