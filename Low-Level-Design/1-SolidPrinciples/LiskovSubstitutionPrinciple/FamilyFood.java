05258090233b588a15a966d0a4ca80a5cb2c348a
satya-supercluster <satya.supercluster@gmail.com>
2025-03-01 16:14:27 +0530

    Refactor DeliveryDiscount example into FamilyFood to enhance Liskov Substitution Principle demonstration with abstract classes

---
+++ Low-Level-Design/1-SolidPrinciples/LiskovSubstitutionPrinciple/FamilyFood.java
@@ -0,0 +1,65 @@
+public class FamilyFood {
+
+    public static void main(String[] args) {
+
+        NonVegOrderFamily family1 = new NonVegFamily();
+        family1.familyType();
+        family1.orderNonVeg();
+        family1.orderVeg();
+
+        PureVegOrderFamily family2 = new PureVegFamily();
+        family2.familyType();
+        family2.orderVeg();
+    }
+}
+
+abstract class Family {
+    abstract void familyType();
+    abstract String getFamilyType();
+    abstract void orderVeg(); 
+}
+
+abstract class PureVegOrderFamily extends Family {
+    @Override
+    public void orderVeg() {
+        System.out.println("Ordering vegetarian food.");
+    }
+}
+
+abstract class NonVegOrderFamily extends Family {
+    abstract void orderNonVeg();
+
+    @Override
+    public void orderVeg() {
+        System.out.println("Ordering vegetarian food.");
+    }
+}
+
+class NonVegFamily extends NonVegOrderFamily {
+    @Override
+    public void familyType() {
+        System.out.println("This is a Non-Vegetarian Family.");
+    }
+
+    @Override
+    public String getFamilyType() {
+        return "Non-Vegetarian Family";
+    }
+
+    @Override
+    public void orderNonVeg() {
+        System.out.println("Ordering meat for Non-Vegetarian Family.");
+    }
+}
+
+class PureVegFamily extends PureVegOrderFamily {
+    @Override
+    public void familyType() {
+        System.out.println("This is a Pure Vegetarian Family.");
+    }
+
+    @Override
+    public String getFamilyType() {
+        return "Pure Vegetarian Family";
+    }
+}
